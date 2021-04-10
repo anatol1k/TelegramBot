712bc1c4c5b782f59b2f60d134fd4d31188feab3
anatol1k <abachakrov@gmail.com>
2021-04-11 02:01:30 +0300

    finally 1.1

--- handlers.py
+++ handlers.py
@@ -9,13 +9,9 @@ def sms(bot, update):
             bot.message.reply_text(f'Напоминаю тебе {bot.message.chat.first_name}!\n Не забудь шампунь и гель для душа')
         if bot.effective_user.id == 317928020:
             bot.message.reply_text(f'Напоминаю тебе {bot.message.chat.first_name}!\n Не забудь взять лопатку и варенье возьми')
-        else:
-            bot.message.reply_text(f'{bot.message.chat.first_name}, что ты тут забыл?!')
         time.sleep(3600)
-
 
 
 # отвечает темже сообщением которое ему прислали
 def parrot(bot, update):
-    pass
-    # bot.message.send_text('Привет')    # отправляем обратно текст который прислал нам пользователь
+    bot.message.reply_text(bot.message.text)    # отправляем обратно текст который прислал нам пользователь
